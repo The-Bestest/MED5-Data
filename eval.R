4c8ff78ada7046b4efbc0f542240df135d7d3b43
Oppelion <56341984+Oppelion@users.noreply.github.com>
2020-12-03 13:23:50 +0100

    Quick cleanup

--- eval.R
+++ eval.R
@@ -1,18 +1,7 @@
-# implicit_agency <- read.csv(file.path(,), stringsAsFactors=FALSE) ### Not used
-# mood <- read.csv(file.path(,), stringsAsFactors=FALSE) ### Not used
-Questionnaire <- read.csv(file.path(,), stringsAsFactors=FALSE)
-
 library(ggplot2)
-### IA_df not used
-# IA_df <- data.frame(TI = implicit_agency$temporal_interval, prcvd = implicit_agency$prcvd_interval, IA1 = implicit_agency$IA1, IA2 = implicit_agency$IA2)
+
+Questionnaire <- read.csv(file.path(,), stringsAsFactors=FALSE)
 Questionnaire_df <- data.frame(SoO = Questionnaire$SoO, SoA = Questionnaire$SoA, Frustration = Questionnaire$Frustration, Proprioception = Questionnaire$Proprioception, Level = Questionnaire$Level)
-## i would recommend merging IA and Questionnaire based on ParticipantID
-## assuming both IA_df and Questionnaire_df have that shared identifier (participantID or PID)
-## df <- merge(IA_df,Questionnaire_df)
-
-
-Q_lvl1 <- filter(Questionnaire_df, lvl == 1)
-Q_lvl2 <- filter(Questionnaire_df, lvl == 2)
 
 ##I wouldn't split this (keep all data in df), use the group functions (or group_by functions) for more powerful and less typing 
 ## e.g. try
@@ -22,36 +11,8 @@ Q_lvl2 <- filter(Questionnaire_df, lvl == 2)
 ## or grouped by lvl
 ##Questionnaire_df %>% group_by(lvl) %>% skim() 
 
-
-#Function to test for normality using shapiro.test and transform the data.
-normality_test <- function(IA1, IA2)
-{
-  #Test for normality and transform the data if needed.
-  shapiro_result <- shapiro.test(IA1)
-  
-  #Test second level for normality and transform the data if needed.
-  shapiro_result_lvl2 <- shapiro.test(IA2)
-
-  #QQ plots for IA1 and IA2
-  QQ1 <- ggplot(implicit_agency, aes(sample = IA1)) +
-    stat_qq() +
-    stat_qq_line()
-  
-  QQ2 <- ggplot(implicit_agency, aes(sample = IA2)) +
-    stat_qq() +
-    stat_qq_line()
-}
-
-##more efficient to do this with a facet 
-##let's assume all data is in df
-##df %>% select(SoO, XA, Proprioception, Frustration,Level) %>% pivot_longer() ... need to better understand what you want to do in tems of variable names but see example below
-##try this: you can remove each pipe symbol (and what comes after) to see how the data gets transformed through the pipe
-##iris %>% select(Sepal.Length, Sepal.Width, Petal.Length, Species) %>% pivot_longer(!Species, values_to="val",names_to="var") %>% ggplot(aes(x = val, y = Species))+geom_boxplot()+facet_wrap(~var)
-
-
-#function for boxplot containing SoO, XA, Frustration, Proprioception
+#function for boxplot containing SoO, SoA, Frustration, Proprioception
 ### This only gives a boxplot for proprioception
-### Changed wording to make it work
 boxplot <- function(SoO, SoA, Frustration, Proprioception, Level)
   {
   plot_SoO <- ggplot(subset(Questionnaire, !is.na(SoO)),aes(x = SoO, y = Level, colour = "#E69F00"))
@@ -63,6 +24,7 @@ boxplot <- function(SoO, SoA, Frustration, Proprioception, Level)
   plot_Proprioception <- ggplot(subset(Questionnaire, !is.na(Proprioception)),aes(x = Proprioception, y = Level, colour = "#F0E442"))
   + geom_boxplot()
 }
+
 ### Use this for the boxplot function
 ChangeThisName <- boxplot(Questionnaire_df$SoO, Questionnaire_df$SoA, Questionnaire_df$Frustation, Questionnaire_df$Proprioception, Questionnaire_df$Level)
 ##more efficient to do this with a facet 
@@ -70,31 +32,6 @@ ChangeThisName <- boxplot(Questionnaire_df$SoO, Questionnaire_df$SoA, Questionna
 ##df %>% select(SoO, XA, Proprioception, Frustration,Level) %>% pivot_longer() ... need to better understand what you want to do in tems of variable names but see example below
 ##try this: you can remove each pipe symbol (and what comes after) to see how the data gets transformed through the pipe
 ##iris %>% select(Sepal.Length, Sepal.Width, Petal.Length, Species) %>% pivot_longer(!Species, values_to="val",names_to="var") %>% ggplot(aes(x = val, y = Species))+geom_boxplot()+facet_wrap(~var)
-
-### This might not be used at all, so didn't test
-Process_IA <- function()
-{
-  IA_means <- vector("double", ncol(implicit_agency), nrow(2))
-  for (i in seq_along(implicit_agency))
-  {
-    IA_means[[i]] <- mean(implicit_agency[[i]])
-  }
-  
-  IA_SD <- vector("double", ncol(implicit_agency), nrow(2))
-  for (i in seq_along(implicit_agency))
-  {
-    IA_SD[[i]] <- sd(implicit_agency[[i]])
-  }
-  
-  IA_medians <- vector("double", ncol(implicit_agency), nrow(2))
-  for (i in seq_along(implicit_agency))
-  {
-    IA_medians[[i]] <- median(implicit_agency[[i]])
-  }
-  
-  #perform a t-test to compare the implicit agency between both levels.
-  implicit_agency$t_result <- t.test(implicit_agency$IA1, implicit_agency$IA2, paired = TRUE, conf.level=0.95)
-}
 
 ##check out the skim command in skimr will do much more for you (see above)
 ### Error in vector("double", ncol(Q_lvl1), nrow(2)) : unused argument (nrow(2))
@@ -161,12 +98,4 @@ correlation <- function()
       }
     }
   }
-}
-
-
-### No working
-scatterPlot_XA_SoO <- function(XA, Frustration, lvl)
-{
-  scatter_XA_SoO <- ggplot(aes(x = XA, y = Frustration, colour = lvl)) +
-    geom_point(alpha = 0.4,  position = position_jitter()) + stat_smooth(method = "lm")
 }
